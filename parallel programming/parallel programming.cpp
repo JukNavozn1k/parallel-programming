7da0a61667a5c66c5e499bdd5bd48caeb7ced640
gleb <deffender1337@gmail.com>
2025-03-12 15:49:15 +0300

    omp test

--- parallel programming/parallel programming.cpp
+++ parallel programming/parallel programming.cpp
@@ -1,20 +1,25 @@
-// parallel programming.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
+﻿#include <iostream>
+#include <omp.h>
 
-#include <iostream>
+int main() {
+    const int size = 1000000;
+    int* array = new int[size];
 
-int main()
-{
-    std::cout << "Hello World!\n";
+    // Инициализация массива
+#pragma omp parallel for
+    for (int i = 0; i < size; i++) {
+        array[i] = i;
+    }
+
+    // Параллельное вычисление суммы элементов массива
+    long long sum = 0;
+#pragma omp parallel for reduction(+:sum)
+    for (int i = 0; i < size; i++) {
+        sum += array[i];
+    }
+
+    std::cout << "Sum of elements: " << sum << std::endl;
+
+    delete[] array;
+    return 0;
 }
-
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
