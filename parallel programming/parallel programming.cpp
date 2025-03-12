a398b0e8b9190f2873f6fb4895a2bf7c4e2631ff
gleb <deffender1337@gmail.com>
2025-03-12 23:17:44 +0300

    afafaf

--- parallel programming/parallel programming.cpp
+++ parallel programming/parallel programming.cpp
@@ -17,43 +17,75 @@ Matrix generateRandomMatrix(int n) {
     return A;
 }
 
-Matrix standardMultiply(const Matrix& A, const Matrix& B) {
+// Последовательное умножение
+Matrix standardMultiplySeq(const Matrix& A, const Matrix& B) {
     int n = A.size();
     Matrix C(n, vector<int>(n, 0));
-    #pragma omp parallel for collapse(3)
-    for (int i = 0; i < n; i++) {
-        for (int k = 0; k < n; k++) {
-            for (int j = 0; j < n; j++) {
+    for (int i = 0; i < n; i++)
+        for (int k = 0; k < n; k++)
+            for (int j = 0; j < n; j++)
                 C[i][j] += A[i][k] * B[k][j];
-            }
-        }
-    }
     return C;
 }
 
-Matrix add(const Matrix& A, const Matrix& B) {
+// Параллельное умножение
+Matrix standardMultiply(const Matrix& A, const Matrix& B) {
     int n = A.size();
     Matrix C(n, vector<int>(n, 0));
-    #pragma omp parallel for collapse(2)
+#pragma omp parallel for collapse(3)
+    for (int i = 0; i < n; i++)
+        for (int k = 0; k < n; k++)
+            for (int j = 0; j < n; j++)
+                C[i][j] += A[i][k] * B[k][j];
+    return C;
+}
+
+// Последовательное сложение
+Matrix addSeq(const Matrix& A, const Matrix& B) {
+    int n = A.size();
+    Matrix C(n, vector<int>(n, 0));
     for (int i = 0; i < n; i++)
         for (int j = 0; j < n; j++)
             C[i][j] = A[i][j] + B[i][j];
     return C;
 }
 
-Matrix subtract(const Matrix& A, const Matrix& B) {
+// Параллельное сложение
+Matrix add(const Matrix& A, const Matrix& B) {
     int n = A.size();
     Matrix C(n, vector<int>(n, 0));
-    #pragma omp parallel for collapse(2)
+#pragma omp parallel for collapse(2)
+    for (int i = 0; i < n; i++)
+        for (int j = 0; j < n; j++)
+            C[i][j] = A[i][j] + B[i][j];
+    return C;
+}
+
+// Последовательное вычитание
+Matrix subtractSeq(const Matrix& A, const Matrix& B) {
+    int n = A.size();
+    Matrix C(n, vector<int>(n, 0));
     for (int i = 0; i < n; i++)
         for (int j = 0; j < n; j++)
             C[i][j] = A[i][j] - B[i][j];
     return C;
 }
 
+// Параллельное вычитание
+Matrix subtract(const Matrix& A, const Matrix& B) {
+    int n = A.size();
+    Matrix C(n, vector<int>(n, 0));
+#pragma omp parallel for collapse(2)
+    for (int i = 0; i < n; i++)
+        for (int j = 0; j < n; j++)
+            C[i][j] = A[i][j] - B[i][j];
+    return C;
+}
+
+// Последовательный алгоритм Штрассена
 Matrix strassenSequential(const Matrix& A, const Matrix& B) {
     int n = A.size();
-    if (n <= STRASSEN_THRESHOLD) return standardMultiply(A, B);
+    if (n <= STRASSEN_THRESHOLD) return standardMultiplySeq(A, B);
 
     int newSize = n / 2;
     Matrix A11(newSize, vector<int>(newSize)), A12(newSize, vector<int>(newSize)),
@@ -73,15 +105,16 @@ Matrix strassenSequential(const Matrix& A, const Matrix& B) {
             B22[i][j] = B[i + newSize][j + newSize];
         }
 
-    Matrix M1 = strassenSequential(add(A11, A22), add(B11, B22));
-    Matrix M2 = strassenSequential(add(A21, A22), B11);
-    Matrix M3 = strassenSequential(A11, subtract(B12, B22));
-    Matrix M4 = strassenSequential(A22, subtract(B21, B11));
-    Matrix M5 = strassenSequential(add(A11, A12), B22);
-    Matrix M6 = strassenSequential(subtract(A21, A11), add(B11, B12));
-    Matrix M7 = strassenSequential(subtract(A12, A22), add(B21, B22));
+    Matrix M1 = strassenSequential(addSeq(A11, A22), addSeq(B11, B22));
+    Matrix M2 = strassenSequential(addSeq(A21, A22), B11);
+    Matrix M3 = strassenSequential(A11, subtractSeq(B12, B22));
+    Matrix M4 = strassenSequential(A22, subtractSeq(B21, B11));
+    Matrix M5 = strassenSequential(addSeq(A11, A12), B22);
+    Matrix M6 = strassenSequential(subtractSeq(A21, A11), addSeq(B11, B12));
+    Matrix M7 = strassenSequential(subtractSeq(A12, A22), addSeq(B21, B22));
 
     Matrix C(n, vector<int>(n));
+#pragma omp parallel for collapse(2)
     for (int i = 0; i < newSize; i++)
         for (int j = 0; j < newSize; j++) {
             C[i][j] = M1[i][j] + M4[i][j] - M5[i][j] + M7[i][j];
@@ -92,79 +125,56 @@ Matrix strassenSequential(const Matrix& A, const Matrix& B) {
     return C;
 }
 
+// Параллельный алгоритм Штрассена
 Matrix strassenParallel(const Matrix& A, const Matrix& B) {
     int n = A.size();
     if (n <= STRASSEN_THRESHOLD) return standardMultiply(A, B);
 
     int newSize = n / 2;
-    Matrix A11(newSize, vector<int>(newSize)), A12(newSize, vector<int>(newSize)),
-        A21(newSize, vector<int>(newSize)), A22(newSize, vector<int>(newSize));
-    Matrix B11(newSize, vector<int>(newSize)), B12(newSize, vector<int>(newSize)),
-        B21(newSize, vector<int>(newSize)), B22(newSize, vector<int>(newSize));
-
-    // Разделение матриц на подматрицы
-    for (int i = 0; i < newSize; i++) {
-        for (int j = 0; j < newSize; j++) {
-            A11[i][j] = A[i][j];
-            A12[i][j] = A[i][j + newSize];
-            A21[i][j] = A[i + newSize][j];
-            A22[i][j] = A[i + newSize][j + newSize];
-            B11[i][j] = B[i][j];
-            B12[i][j] = B[i][j + newSize];
-            B21[i][j] = B[i + newSize][j];
-            B22[i][j] = B[i + newSize][j + newSize];
-        }
-    }
-
     Matrix M1, M2, M3, M4, M5, M6, M7;
 
-    // Параллелим рекурсивные вычисления
 #pragma omp parallel sections
     {
 #pragma omp section
-        M1 = strassenParallel(add(A11, A22), add(B11, B22));
+        M1 = strassenParallel(add(A, A), add(B, B));
 
 #pragma omp section
-        M2 = strassenParallel(add(A21, A22), B11);
+        M2 = strassenParallel(add(A, A), B);
 
 #pragma omp section
-        M3 = strassenParallel(A11, subtract(B12, B22));
+        M3 = strassenParallel(A, subtract(B, B));
 
 #pragma omp section
-        M4 = strassenParallel(A22, subtract(B21, B11));
+        M4 = strassenParallel(A, subtract(B, B));
 
 #pragma omp section
-        M5 = strassenParallel(add(A11, A12), B22);
+        M5 = strassenParallel(add(A, A), B);
 
 #pragma omp section
-        M6 = strassenParallel(subtract(A21, A11), add(B11, B12));
+        M6 = strassenParallel(subtract(A, A), add(B, B));
 
 #pragma omp section
-        M7 = strassenParallel(subtract(A12, A22), add(B21, B22));
+        M7 = strassenParallel(subtract(A, A), add(B, B));
     }
 
-    // Сборка результата
     Matrix C(n, vector<int>(n));
 #pragma omp parallel for collapse(2)
-    for (int i = 0; i < newSize; i++) {
+    for (int i = 0; i < newSize; i++)
         for (int j = 0; j < newSize; j++) {
             C[i][j] = M1[i][j] + M4[i][j] - M5[i][j] + M7[i][j];
-            C[i][j + newSize] = M3[i][j] + M5[i][j];
-            C[i + newSize][j] = M2[i][j] + M4[i][j];
-            C[i + newSize][j + newSize] = M1[i][j] - M2[i][j] + M3[i][j] + M6[i][j];
         }
-    }
+
     return C;
 }
+
+
 int main() {
-    int n = 1024;
+    int n = 2;
     Matrix A = generateRandomMatrix(n);
     Matrix B = generateRandomMatrix(n);
 
     Matrix C_std, C_seq, C_par;
     double time_std, time_seq, time_par;
-
-    // omp_set_num_threads(12); // Гарантируем 12 потоков
 
     // Стандартное умножение
     auto start_std = chrono::high_resolution_clock::now();
